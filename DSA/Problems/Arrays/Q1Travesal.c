f80882f9b9f022c99bd1fe0714f60908a8523404
Anubhav Saini <anubhav.21b1531122@abes.ac.in>
2022-12-27 11:25:06 +0530

    Second Commit

---
+++ DSA/Problems/Arrays/Q1Travesal.c
@@ -0,0 +1,12 @@
+//Program for traversing array elements.
+
+#include<stdio.h>
+int main(){
+    int i,size;
+    int a[]={1,2,3,4};
+    printf("the element are\n");
+    for(i=0;i<4;i++){
+        printf(",%d",a[i]);
+    }
+
+}
